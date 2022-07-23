e3947f095dc365b44543acd3ebb467c1e54a5190
Luana Batista Campião <luana.campiao@yahoo.com.br>
2022-07-23 13:42:56 -0300

    Adição de menu opções

--- clientes-python-main/clientes-python-main/controllers/controllerAplicacao.py
+++ clientes-python-main/clientes-python-main/controllers/controllerAplicacao.py
@@ -2,13 +2,27 @@ import models.banco as banco
 import views.formulario as view
 
 # Controller - a validação
-def validar_login(usuario_digitado, senha_digitado):
+def validar_login(usuario_completo):
     usuario_BD = banco.model_usuario()
     senha_BD = banco.model_senha()
-    if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
-        print("pode entrar")
+    if usuario_completo[0] == usuario_BD and usuario_completo[1] == senha_BD:
+        view.exibir_mensagem("pode entrar")
+        opcoes_menu()
     else:
-        print("usuário ou senha inválido")
+        view.exibir_mensagem("usuário ou senha inválido")
+        iniciar()
 
-def iniciar():
-    view.formulario_login()
+def iniciar(): 
+    #usuario_completo = view.formulario_login()
+    #validar_login(usuario_completo) ou mais enxuto como abaixo
+    validar_login(view.formulario_login())
+    
+def opcoes_menu():
+    opcao = view.menu()
+    if opcao =='1':
+        print("Cadastro de clientes")
+    elif opcao =='2':
+        print("Listagem de clientes")
+    else:
+        view.exibir_mensagem("Sistema finalizado")
+        exit()
--- clientes-python-main/clientes-python-main/views/formulario.py
+++ clientes-python-main/clientes-python-main/views/formulario.py
@@ -1,7 +1,20 @@
-import controllers.validacao as validacao
+
 # View - o que vai para o usuário
 def formulario_login():
     usuario_digitado = input("Informe o seu usuário: ")
     senha_digitado = input("Informe sua senha: ")
-    validacao.validar_login(usuario_digitado, senha_digitado)
-
+    usuario_completo = [usuario_digitado, senha_digitado]
+    return usuario_completo
+def exibir_mensagem(texto):
+        print("\n\n")
+        print("===============")
+        print(texto)
+        print("================")
+        print("\n\n")
+        
+def menu():
+    print("1 - Para cadastrar novo cliente")
+    print("2 - Para listar n todos clietnes")
+    print("3 - para sair")
+    opcao = input("digite a opção")
+    return opcao
